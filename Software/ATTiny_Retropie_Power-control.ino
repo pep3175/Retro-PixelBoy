cae7fe4ef9dd9bed9a445d1607ace55ef13da7b7
Pep3175 <59727461+pep3175@users.noreply.github.com>
2020-06-29 10:33:39 +0300

    Update ATTiny_Retropie_Power-control.ino

--- Software/ATTiny_Retropie_Power-control.ino
+++ Software/ATTiny_Retropie_Power-control.ino
@@ -1,8 +1,8 @@
-const int powerCtrl = 0;
-const int pushButton = 1;
-const int gpioShutdown = 2;
-const int gpioPoweroff = 3;
-const int powerRasp = 4;
+const int powerCtrl = 0;      //Controle l'alim de l'ATTiny
+const int powerRasp = 4;      //Contrôle l'alim du RPI
+const int gpioShutdown = 2;   //Commande l'extinction du RPI
+const int gpioPoweroff = 3;   //Signale que le RPI est éteint
+const int pushButton = 1;     //Bouton PWR
 
 int buttonState = 0;
 int raspOff = 0;
@@ -17,32 +17,48 @@ void setup() {
   pinMode(pushButton, INPUT);
 
   digitalWrite(powerCtrl, LOW);
+  digitalWrite(powerRasp, LOW);
+  digitalWrite(gpioShutdown, LOW);
 
-  delay(2000);
+  delay(2000);    //Tempo pression boutton avant démarrage
 
   digitalWrite(powerCtrl, HIGH);
   digitalWrite(powerRasp, HIGH);
+  
+  //delay(2000);
+  
+  delay(20000);    //Attendre que le RPI ai bien démarré avant de permettre l'extinction
+  /*raspOff = digitalRead(gpioPoweroff);
+  while (raspOff == HIGH) {
+    delay(500);
+    raspOff = digitalRead(gpioPoweroff);
+  }*/
 }
 
 void loop() {
   buttonState = digitalRead(pushButton);
   raspOff = digitalRead(gpioPoweroff);
-  
-  if (buttonState == HIGH) {
+
+  //Appui environ 1.5s sur le bouton pour commander l'extinction du RPI  
+  if (buttonState == LOW) {
     count1++;
-    if (count1 == 30) digitalWrite(gpioShutdown, HIGH);
+    if (count1 == 30) {
+      digitalWrite(gpioShutdown, HIGH);
+      delay(200);
+      digitalWrite(gpioShutdown, LOW);
+    }
   } else {
-    digitalWrite(gpioShutdown, LOW);
     count1 = 0;
   }
 
+  //Lorsque le RPI est éteint raspOff passe à HIGH
   if (raspOff == HIGH) {
     count2++;
-    if (count2 == 30) {
-      delay(2000);
-      digitalWrite(powerRasp, LOW);
+    if (count2 == 20) {
       delay(1000);
-      digitalWrite(powerCtrl, LOW);
+      digitalWrite(powerRasp, LOW); //On coupe l'alim du RPI
+      delay(1000);
+      digitalWrite(powerCtrl, LOW); //On coupe l'alim du ATTiny
     }
   } else {
     count2 = 0;
